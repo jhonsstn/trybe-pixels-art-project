2620f5377ade80a3c6e82eee704ef3808b3f3b57
Jhon Santana <jhonsstn@gmail.com>
2021-12-09 21:11:30 -0300

    add botao e finalizados testes obrigatorios

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
-window.onload = function () {};
-
-let firstColor = document.querySelector('.black-square');
+const firstColor = document.querySelector('.black-square');
 firstColor.classList.add('selected');
-let cor = 'black';
+let color = 'black';
 
-let colorPalette = document.querySelectorAll('.color');
+const colorPalette = document.querySelectorAll('.color');
 
 document.querySelectorAll('.color').forEach((item) => {
   item.addEventListener('click', (event) => {
-    for (let i = 0; i < colorPalette.length; i++) {
+    for (let i = 0; i < colorPalette.length; i += 1) {
       if (colorPalette[i].classList.contains('selected')) {
         colorPalette[i].classList.remove('selected');
       }
     }
-    event.target.classList.add('selected');
-    cor = window
-      .getComputedStyle(event.target)
+    const colorButton = event.target;
+    colorButton.classList.add('selected');
+    color = window
+      .getComputedStyle(colorButton)
       .getPropertyValue('background-color');
   });
 });
 
 document.querySelectorAll('.pixel').forEach((item) => {
-  item.addEventListener('click', (event) => {
-    event.target.style.backgroundColor = cor;
+  item.addEventListener('click', () => {
+    if (item.style.backgroundColor !== color) {
+      item.style.backgroundColor = color;
+    } else {
+      item.style.backgroundColor = 'white';
+    }
+  });
+});
+
+document.getElementById('clear-board').addEventListener('click', () => {
+  document.querySelectorAll('.pixel').forEach((item) => {
+    if (item.style.backgroundColor !== 'white') {
+      item.style.backgroundColor = 'white';
+    }
+    return;
   });
 });
